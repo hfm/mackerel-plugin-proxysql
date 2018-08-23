8c26a97ad3895cec898fd0260e845735f8a5ac0a
OKUMURA Takahiro <hfm.garden@gmail.com>
2018-08-23 20:26:51 +0900

    Fix import path

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-import "github.com/mackerelio/mackerel-agent-plugins/mackerel-plugin-proxysql/lib"
+import "github.com/hfm/mackerel-plugin-proxysql/lib"
 
 func main() {
 	mpproxysql.Do()
